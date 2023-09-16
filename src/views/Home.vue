5a571eab4cea28696a5d832fa76f373181c53b2f
DESKTOP-VJC7U4F\chbee <chbee318@gmail.com>
2023-09-17 00:44:33 +0800

    Created the search bar in home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,27 @@
 <template>
-  <main>
-    <h1 class="bg-blue-200">App.js</h1>
-  </main>
+  <div class="flex flex-col p-8">
+    <input
+      type="text"
+      class="rounded border-2 border-gray-200 w-full p-3"
+      placeholder="Search for Meals"
+    />
+
+    <div class="flex justify-center gap-5 mt-3">
+      <router-link
+        :to="{ name: 'byLetter', params: { letter } }"
+        v-for="letter of letters"
+        :key="letter"
+      >
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
 </template>
-<script setup></script>
+<script setup>
+import { computed } from 'vue'
+import store from '../store'
+import { RouterLink } from 'vue-router'
+
+const meals = computed(() => store.state.meals)
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
+</script>
