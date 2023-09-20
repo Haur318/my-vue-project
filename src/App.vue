1552b73a66b86212ce7dfcbe9fa9331c38af18ab
Haur <chunhaur.lim@silverlakeaxis.com>
2023-09-20 12:01:50 +0800

    Created the design for mobile view

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,80 @@
 <template>
-  <div class="bg-gray-100 h-full">
-    <header class="bg-white shadow h-20 flex justify-between item-center items-stretch">
-      <router-link
-        :to="{ name: 'home' }"
-        class="inline-flex items-center px-5 h-full hover:bg-sky-400 hover:text-white italic"
-        >Home</router-link
-      >
-      <div>
-        <router-link :to="{ name: 'home' }" class="inline-flex items-center px-2 h-full italic"
-          >Search Meals</router-link
-        >
-        <router-link :to="{ name: 'home' }" class="inline-flex items-center px-2 h-full italic"
-          >Meals By Letter</router-link
-        >
-        <router-link :to="{ name: 'home' }" class="inline-flex items-center px-2 h-full italic"
-          >Meals By Ingredients</router-link
-        >
+  <div class="bg-gradient-to-r from-teal-500 to-blue-800">
+    <nav class="
+        container
+        px-6
+        py-4
+        mx-auto
+        md:flex md:justify-between md:items-center
+      ">
+      <div class="flex items-center justify-between">
+        <router-link to="/" class="
+            text-xl
+            font-bold
+            text-gray-100
+            md:text-2xl
+          ">Logo
+        </router-link>
+        <!-- Mobile menu button -->
+        <div @click="toggleNav" class="flex md:hidden">
+          <button type="button" class="
+              text-gray-100
+              hover:text-gray-400
+              focus:outline-none focus:text-gray-400
+            ">
+            <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+              <path fill-rule="evenodd"
+                d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z">
+              </path>
+            </svg>
+          </button>
+        </div>
       </div>
-    </header>
-    <main>
-      <router-view />
-    </main>
+
+      <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+      <ul :class="showMenu ? 'flex' : 'hidden'" class="
+          flex-col
+          mt-3
+          space-y-4
+          md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0
+        ">
+        <li class="text-gray-100" v-for="(navItem, i) in navMenu" :key="navItem">
+          <router-link :to="{ name: navMenu[i].name }" class="block p-3">
+            {{ navMenu[i].title }}
+          </router-link>
+        </li>
+      </ul>
+    </nav>
   </div>
+  <main>
+    <router-view />
+  </main>
 </template>
+<script>
+import { ref } from 'vue';
+export default {
+  data() {
+    return {
+      navMenu: [
+        {
+          name: "home",
+          title: "Search Meals"
+        },
+        {
+          name: "home",
+          title: "Meals By Letter"
+        },
+        {
+          name: "home",
+          title: "Meals By Ingredients"
+        }
+      ]
+    }
+  },
+  setup() {
+    let showMenu = ref(false);
+    const toggleNav = () => (showMenu.value = !showMenu.value);
+    return { showMenu, toggleNav };
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex flex-col p-8">
+  <div class="flex flex-col px-6 py-3" >
     <input
       type="text"
       class="rounded-full border-2 border-gray-200 w-full p-3"
       placeholder="Search for Meals"
     />
 
-    <div class="flex flex-wrap justify-center gap-3 mt-5 italic">
+    <div class="flex flex-wrap justify-center gap-3 mt-5 italic invisible md:visible">
       <router-link
         :to="{ name: 'byLetter', params: { letter } }"
         v-for="letter of letters"
