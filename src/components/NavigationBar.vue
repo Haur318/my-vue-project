4aa01ae28e4faa0b7151ec5e9f85a0e77d67e6df
Haur <chunhaur.lim@silverlakeaxis.com>
2023-10-04 11:30:57 +0800

    Added contents and scoll-bar in hompage

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -2,30 +2,24 @@
   <div class="bg-gradient-to-r from-teal-500 to-blue-800">
     <nav class="px-6 py-4 mx-auto md:flex md:justify-between md:items-center">
       <div class="flex items-center justify-between">
-        <router-link to="/" class="text-xl font-bold text-gray-100 md:text-2xl"
-          ><img src="/src/assets/mealIcon.png" class="w-12 h-12 inline-block" /> {{ iconTitle }}
+        <router-link @click="showMenu = false" to="/" class="text-xl font-bold text-gray-100 md:text-2xl"><img
+            src="/src/assets/mealIcon.png" class="w-12 h-12 inline-block" /> {{ iconTitle }}
         </router-link>
         <!-- Mobile menu button -->
         <div @click="toggleNav" class="flex md:hidden">
-          <button
-            type="button"
-            class="text-gray-100 hover:text-gray-400 focus:outline-none focus:text-gray-400"
-          >
+          <button type="button" class="text-gray-100 hover:text-gray-400 focus:outline-none focus:text-gray-400">
             <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
-              <path
-                fill-rule="evenodd"
-                d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-              ></path>
+              <path fill-rule="evenodd"
+                d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z">
+              </path>
             </svg>
           </button>
         </div>
       </div>
 
       <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
-      <ul
-        :class="showMenu ? 'flex' : 'hidden'"
-        class="flex-col mt-3 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0"
-      >
+      <ul :class="showMenu ? 'flex' : 'hidden'"
+        class="flex-col mt-3 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
         <li class="text-gray-100" v-for="(navItem, i) in navMenu" :key="navItem">
           <router-link :to="{ name: navMenu[i].name }" class="block p-3">
             {{ navMenu[i].title }}
@@ -59,7 +53,8 @@ export default {
   },
   watch: {
     $route() {
-      this.showMenu = !this.showMenu
+      this.showMenu = false
+
     }
   },
   mounted() {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,40 @@
-<template>
-  <div></div>
+<template v-if="mealsByCategory">
+  <div v-for="meals in mealsByCategory" :key="meals">
+    <h3 class="text-xl font-bold px-5 py-2" data-aos="fade-left" data-aos-duration="1000">
+      {{ meals[0].strCategory ? meals[0].strCategory : 'N/A' }}
+    </h3>
+    <div class="flex flex-nowrap overflow-x-auto no-scrollbar" data-aos="zoom-in" data-aos-duration="1000">
+      <div v-for="meal in meals" :key="meal" class="p-3 m-2 flex-shrink-0">
+        <router-link to="/">
+          <img :src="meal.strMealThumb" :alt="meal.strMeal" class="rounded-lg w-50 h-32 md:h-72" />
+        </router-link>
+      </div>
+    </div>
+  </div>
 </template>
-<script setup></script>
+<script>
+import axiosClient from '../axiosClient'
+export default {
+  setup() {
+    return {
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
+  },
+  data() {
+    return {
+      mealsByCategory: []
+    }
+  },
+  async mounted() {
+    await axiosClient.get('/meals.json').then((res) => {
+      let mealRes = res.data.meals
+      let result = Object.values(
+        mealRes.reduce((acc, curr) => (
+          (acc[curr.strCategory] = acc[curr.strCategory] || []).push(curr), acc
+        ), {})
+      )
+      this.mealsByCategory = [...result]
+    })
+  }
+}
+</script>
